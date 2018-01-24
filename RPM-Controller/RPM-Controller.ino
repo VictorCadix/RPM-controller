7965d47bb00f5a7bc6cec96e4cec286789704362
VictorCadix <31282394+VictorCadix@users.noreply.github.com>
2018-01-24 23:40:27 +0100

    Funcionamiento motor añadido

--- RPM-Controller/RPM-Controller.ino
+++ RPM-Controller/RPM-Controller.ino
@@ -1,4 +1,7 @@
 #define pinIR 2
+#define pinINA 8
+#define pinINB 9
+#define pinMotorPWM 10
 
 volatile int vueltas;
 
@@ -12,12 +15,20 @@ void setup() {
   pinMode(pinIR, INPUT);
   attachInterrupt(0, doIR, FALLING); //Pin 2
 
+  //Setup Motor
+  pinMode(pinINA,OUTPUT);
+  pinMode(pinINB,OUTPUT);
+  pinMode(pinMotorPWM,OUTPUT);
+
+  //Variable initialisation
   vueltas = 0;
   
 }
 
 void loop() {
-  
+  digitalWrite(pinMotorPWM,HIGH);
+  digitalWrite(pinINA,HIGH);
+  digitalWrite(pinINB,LOW);
   Serial.println(vueltas);
   //Serial.print("/");
  
